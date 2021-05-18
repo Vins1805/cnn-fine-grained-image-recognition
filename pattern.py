709eeb20892911e54bb2532ccf2f081c3ac2fcb8
Vincent <s0551489@htw-berlin.de>
2021-05-18 13:36:14 +0200

    .

--- pattern.py
+++ pattern.py
@@ -2,21 +2,43 @@ import torch
 import cv2
 import numpy as np
 from datasets.dataset import CUBDataset
+from torchvision import transforms
 
 cub = CUBDataset()
 
 conv = torch.hub.load('pytorch/vision:v0.9.0', 'vgg19', pretrained=True)
 
 img, label = cub[2]
-img = torch.Tensor(img).unsqueeze(0)
 print("image shape:", img.shape)
+#img = transforms.ToTensor()(img)
 
 
 
 # show original image
-base_img = np.array(img.squeeze(0).permute(1,2,0)).astype(np.uint8)
+base_img = img.transpose(1,2,0).astype(np.uint8)
 print("base image shape:", base_img.shape)
 cv2.imshow("base", base_img)
+
+
+
+# normalization
+img = torch.Tensor(img)
+mean, std = img.mean([1,2]), img.std([1,2])
+print("Mean:", mean, "\nStd:", std)
+print("image shape:", img.shape)
+img = transforms.Normalize(mean, std)(img)
+print(img)
+
+# show normalized image
+norm_img = np.array(img).transpose(1,2,0).astype(np.uint8)
+print("norm image shape:", norm_img.shape)
+#cv2.imshow("norm", norm_img)
+
+# after normalization mean should be 0 and std be 1
+mean, std = img.mean([1,2]), img.std([1,2])
+print("Mean:", mean, "\nStd:", std)
+
+img = img.unsqueeze(0)
 
 
 
@@ -54,7 +76,7 @@ for row in range(square_size):
         else:
             row_img = _img
             
-print(c_pattern.shape)
+#print(c_pattern.shape)
 c_pattern = cv2.resize(c_pattern, [i*4 for i in c_pattern.shape])
 #cv2.imshow("pattern", c_pattern)
 
@@ -62,14 +84,16 @@ c_pattern = cv2.resize(c_pattern, [i*4 for i in c_pattern.shape])
 
 # mean of all patterns to get most interessting patter images
 p = patterns[0,:,:,:].detach().numpy().astype(np.uint8)
-total_mean = np.mean(p)
+bias = .5
+total_mean = np.mean(p) +  bias
+print("Total pattern mean:", total_mean)
 
 i = 0
 store = list()
 for i in range(p.shape[0]):
     if np.mean(p[i]) > total_mean:
         store.append(np.pad(p[i], [(0, 1), (0, 1)], mode='constant', constant_values=(255)))
-print(len(store))
+print("Patters above mean:", len(store))
 
 square = int(np.sqrt(len(store)))
 c_pattern = None
